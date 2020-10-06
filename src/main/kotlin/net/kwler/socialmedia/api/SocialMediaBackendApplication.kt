a5777f644b6987a28b7ce8f217d74ed06aa34d48
LM Bibera <lyndon.bibera@headhuntr.io>
2020-10-06 16:20:15 +0800

    Generate project

---
+++ src/main/kotlin/net/kwler/socialmedia/api/SocialMediaBackendApplication.kt
@@ -0,0 +1,11 @@
+package net.kwler.socialmedia.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialMediaBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialMediaBackendApplication>(*args)
+}
---
+++ src/test/kotlin/net/kwler/socialmedia/api/SocialMediaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.kwler.socialmedia.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialMediaBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
