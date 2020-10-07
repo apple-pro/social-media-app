ff9d17d7201dc2309ac12785e25e0e1f6fd1b635
LM Bibera <lyndon.bibera@headhuntr.io>
2020-10-07 17:31:10 +0800

    Add ios app

---
+++ ios/SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by StartupBuilder.INFO on 10/7/20.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
