c6e250fca522df4194ed773f73fd1df4d6c73d41
LM Bibera <lyndon.bibera@headhuntr.io>
2020-10-07 21:06:24 +0800

    #1: changed my mind on the auth flow, i know i got this incredibly wrong, registration should be done not on the resource server, but on the auth server (cognito)

--- ios/SocialMedia/ContentView.swift
+++ ios/SocialMedia/ContentView.swift
@@ -6,11 +6,62 @@
 //
 
 import SwiftUI
+import Combine
+import Amplify
+
 
 struct ContentView: View {
+    
+    @State var username = ""
+    @State var password = ""
+    
+    @State var registrationTask: AnyCancellable?
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Form {
+                
+                TextField("Username", text: $username)
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                    .disableAutocorrection(true)
+                
+                SecureField("Password", text: $password)
+                
+                Button("Register") {
+                    registrationTask = signUp(username: username, password: password, email: username)
+                }
+                
+                if registrationTask != nil {
+                    Button("Cancel") {
+                        registrationTask?.cancel()
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+    
+    func signUp(username: String, password: String, email: String) -> AnyCancellable {
+        let userAttributes = [AuthUserAttribute(.email, value: email)]
+        let options = AuthSignUpRequest.Options(userAttributes: userAttributes)
+        
+        return Amplify.Auth.signUp(username: username, password: password, options: options)
+            .resultPublisher
+            .sink {
+                if case let .failure(authError) = $0 {
+                    print("An error occurred while registering a user \(authError)")
+                }
+            }
+            receiveValue: { signUpResult in
+                if case let .confirmUser(deliveryDetails, _) = signUpResult.nextStep {
+                    print("Delivery details \(String(describing: deliveryDetails))")
+                } else {
+                    print("SignUp Complete")
+                }
+                
+            }
     }
 }
 
--- ios/SocialMedia/SocialMediaApp.swift
+++ ios/SocialMedia/SocialMediaApp.swift
@@ -6,12 +6,34 @@
 //
 
 import SwiftUI
+import Amplify
+import AmplifyPlugins
 
 @main
 struct SocialMediaApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        do {
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            try Amplify.configure()
+            print("Amplify configured with auth plugin")
+        } catch {
+            print("Failed to initialize Amplify with \(error)")
+        }
+        
+        return true
+    }
+    
 }
