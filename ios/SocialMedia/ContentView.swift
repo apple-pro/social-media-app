513cccb16929610f6146d8734849c419450f0745
LM Bibera <lyndon.bibera@headhuntr.io>
2020-10-08 11:34:59 +0800

    #1 - finally displayed the user's name

--- ios/SocialMedia/ContentView.swift
+++ ios/SocialMedia/ContentView.swift
@@ -7,41 +7,99 @@
 
 import SwiftUI
 import Amplify
-
+import Combine
 
 struct ContentView: View {
     
-    @State var userLoggedIn = false
+    @State var loginState: AppAuthState = .unknown
+    @State var authUpdates: UnsubscribeToken?
     
     var body: some View {
         ZStack {
-            if userLoggedIn {
+            switch loginState {
+            case .unknown:
+                PleaseWait(loginState: $loginState)
+                
+            case .authenticated:
                 UserProfile()
-            } else {
+
+            default:
                 Login()
             }
-        }.onAppear {
-            listenToAuthEvents()
+        }
+        .onAppear {
+            startListeningToAuthEvents()
+        }
+        .onDisappear {
+            stopListeningToAuthEvents()
         }
     }
     
-    func listenToAuthEvents() {
-        Amplify.Hub.listen(to: .auth) { payload in
+    
+    func startListeningToAuthEvents() {
+        
+        authUpdates = Amplify.Hub.listen(to: .auth) { payload in
             switch payload.eventName {
             case HubPayload.EventName.Auth.signedIn:
                 print("User signed in")
-                userLoggedIn = true
+                loginState = .authenticated
                 
             case HubPayload.EventName.Auth.sessionExpired:
                 print("Session expired")
-                userLoggedIn = false
+                loginState = .unauthenticated
                 
             case HubPayload.EventName.Auth.signedOut:
                 print("User signed out")
-                userLoggedIn = false
+                loginState = .unauthenticated
                 
             default:
+                loginState = .authenticated
                 break
+            }
+        }
+    }
+    
+    
+    func stopListeningToAuthEvents() {
+        
+        if let updates = authUpdates {
+            Amplify.Hub.removeListener(updates)
+        }
+    }
+}
+
+enum AppAuthState {
+    case unknown, authenticated, unauthenticated
+}
+
+struct PleaseWait: View {
+    
+    @Binding var loginState: AppAuthState
+    
+    var body: some View {
+        
+        VStack {
+            Text("Please wait...")
+            
+            Button("Retry") {
+                
+            }
+        }
+        .onAppear {
+            fetchAttributes()
+        }
+    }
+    
+    func fetchAttributes() {
+        
+        Amplify.Auth.fetchUserAttributes() { result in
+            switch result {
+            case .success(let attributes):
+                print("User attributes - \(attributes)")
+                loginState = .authenticated
+            case .failure(let error):
+                print("Fetching user attributes failed with error \(error)")
+                loginState = .unauthenticated
             }
         }
     }
--- ios/SocialMedia/Views/UserProfile.swift
+++ ios/SocialMedia/Views/UserProfile.swift
@@ -6,10 +6,58 @@
 //
 
 import SwiftUI
+import Amplify
 
 struct UserProfile: View {
+    
+    @State var errorMessage = ""
+    @State var dataLoaded = false
+    @State var givenName = ""
+    
     var body: some View {
-        Text("This is the User Profile")
+        Form {
+            
+            if !errorMessage.isEmpty {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .font(.caption)
+            }
+            
+            if dataLoaded {
+                
+                TextField("Given Name", text: $givenName)
+                
+            } else {
+                
+                Text("Please Wait...")
+                
+                Button("Try Again?") {
+                    fetchAttributes()
+                }
+            }
+            
+        }.onAppear {
+            fetchAttributes()
+        }
+    }
+    
+    func fetchAttributes() {
+        
+        Amplify.Auth.fetchUserAttributes() { result in
+            switch result {
+            case .success(let attributes):
+                print("User attributes - \(attributes)")
+                dataLoaded = true
+                
+                if let givenNameAttribute = attributes.first (where: { a in a.key == .givenName } ) {
+                    givenName = givenNameAttribute.value
+                }
+            case .failure(let error):
+                print("Fetching user attributes failed with error \(error)")
+                dataLoaded = false
+                errorMessage = error.localizedDescription
+            }
+        }
     }
 }
 
